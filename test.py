1f6b13964107cf363a41854fb1015039233b0ffe
atvf000 <askh24@mail.ru>
2019-12-14 04:23:37 +0300

    add test

--- test.py
+++ test.py
@@ -84,8 +84,9 @@ class Test_1(unittest.TestCase):
         )
         self.assertEqual(user_storage2, user_storage)
 
+
 class Test_2(unittest.TestCase):
-    def test_newgame(self):
+    def test_nextstep(self):
         request = {
             "meta": {
 
@@ -159,8 +160,9 @@ class Test_2(unittest.TestCase):
         )
         self.assertEqual(user_storage2["step"], user_storage["step"] + 1)
 
+
 class Test_3(unittest.TestCase):
-    def test_newgame(self):
+    def test_init(self):
         request = {
             "meta": {
 
@@ -232,4 +234,82 @@ class Test_3(unittest.TestCase):
             ensure_ascii=False,
             indent=2
         )
-        self.assertNotEqual(user_storage["matrix"],user_storage2["matrix"])
+        self.assertNotEqual(user_storage["matrix"], user_storage2["matrix"])
+
+
+class Test_4(unittest.TestCase):
+    def test_init(self):
+        request = {
+            "meta": {
+
+                "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
+                "interfaces": {
+                    "account_linking": {},
+                    "payments": {},
+                    "screen": {}
+                },
+                "locale": "ru-RU",
+                "timezone": "UTC"
+            },
+            "request": {
+                "command": "отметить а 0",
+                "nlu": {
+                    "entities": [],
+                    "tokens": []
+                },
+                "original_utterance": "",
+                "type": "SimpleUtterance"
+            },
+            "session": {
+                "message_id": 0,
+                "new": True,
+                "session_id": "d62bb4d3-984a4120-9ec254cc-96e71d84",
+                "skill_id": "241f1b35-e113-4472-b31f-9183918c6e91",
+                "user_id": "56ED627ECBA15CD74D5CF77980EF2354C895831C9D6709D0652EF7CE32735EB6"
+            },
+            "version": "1.0"
+        }
+
+        user_storage = {
+            "user_id": "56ED627ECBA15CD74D5CF77980EF2354C895831C9D6709D0652EF7CE32735EB6",
+            "users_turn": True,
+            "matrix": [[0 for j in range(10)] for i in range(10)],
+            "open_cells": [[0 for j in range(10)] for i in range(10)],
+            "step": 0
+
+        }
+
+        response = {
+            "version": "1.0",
+            "session": {
+                "message_id": 0,
+                "new": True,
+                "session_id": "d62bb4d3-984a4120-9ec254cc-96e71d84",
+                "skill_id": "241f1b35-e113-4472-b31f-9183918c6e91",
+                "user_id": "56ED627ECBA15CD74D5CF77980EF2354C895831C9D6709D0652EF7CE32735EB6"
+            },
+            "response": {
+                "end_session": False,
+                "text": "Привет! Давай сыграем в Сапера!"
+                        " \n- Число в ячейке показывает, сколько мин скрыто вокруг данной ячейки\n"
+                        "\n- Если рядом с открытой ячейкой есть пустая ячейка, то она откроется автоматически.\n"
+                        "\n- Если вы открыли ячейку с миной, то игра проиграна."
+            }
+        }
+
+        alice_request = AliceRequest(request)
+
+        alice_response = AliceResponse(alice_request)
+
+        alice_response, user_storage2 = handle_dialog(
+            alice_request, alice_response, user_storage
+        )
+
+        json_str = json.dumps(
+            response,
+            ensure_ascii=False,
+            indent=2
+        )
+        typeC = user_storage2["open_cells"]
+
+        self.assertTrue(typeC[0][0] == 2)
